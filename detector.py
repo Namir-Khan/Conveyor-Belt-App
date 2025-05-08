03c8d289efd7100748d00859cac551ac1569e7f6
Namir Khan <Namirk16@gmail.com>
2025-05-08 23:34:18 +0530

    2.1 Update

--- detector.py
+++ detector.py
@@ -6,6 +6,18 @@ class Detector:
 
     def detect(self, frame):
         results = self.model(frame, conf=0.4, save=False, show=False, verbose=False)
-        if results and results[0].boxes.cls.numel() > 0:
-            return int(results[0].boxes.cls[0].item())
-        return "No object"
+
+        if results:
+            boxes = results[0].boxes
+
+        # if results and results[0].boxes.cls.numel() > 0:
+        #     class_id = int(results[0].boxes.cls[0].item())
+
+        if boxes.cls.numel() > 0:
+            max_conf_idx = boxes.conf.argmax().item()
+            class_id = int(boxes.cls[max_conf_idx].item())
+            class_name = self.model.names[class_id]
+        else:
+            class_name = "No object"
+
+        return class_name
